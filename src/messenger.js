092776a98be6acba756d09a27312af2876559c28
dan <williamdanielquinones@gmail.com>
2019-03-19 10:47:09 -0400

    boit talks more

--- src/messenger.js
+++ src/messenger.js
@@ -23,30 +23,82 @@ class Messenger {
     static spongebobMeme(bot, username, text, channel, submittedAt){
         const acknowledge = 'slackboit ';
         let lowered = text.toLowerCase();
-        if (lowered.startsWith(acknowledge)) {
-            let split = text.split('');
-            split.splice(0, 10);
-
-            let joined = split.map((char, index) => {
-                if (index % 2 === 0) return char.toLowerCase();
-                else return char.toUpperCase();
-            }).join('');
-
-            return bot.postMessage(channel, joined, {});
-        }
+        if (lowered.startsWith(acknowledge)) return bot.postMessage(channel, IDoThings.spongebobMemeify(lowered), {});     
     }
 
     static goodBoit(bot, username, text, channel, submittedAt){
         const acknowledge = "good boit";
         let lowered = text.toLowerCase();
-        if (lowered.includes(acknowledge)) return bot.postMessage(channel, "tHaNkS DaD", {});
+        if (lowered.includes(acknowledge)) return bot.postMessage(channel, IDoThings.spongebobMemeify("thanks dad"), {});
+    }
+
+    static greetingBoit(bot, username, text, channel, submittedAt){
+        const acknowledge = ["good morning", "morno", "hello"];
+        let lowered = text.toLowerCase();
+        if (acknowledge.includes(lowered))
+        {
+            let message = '';
+            if(lowered.includes('good morning') || lowered.includes('morno'))
+            {
+                message = "good morning "
+            }else
+            {
+                message = "greetings "
+            }
+            message += " " + IDoThings.getName(username);
+            return bot.postMessage(channel, IDoThings.spongebobMemeify(message), {});
+        } 
+    }
+}
+
+class IDoThings {
+    //this functions expects a string
+    static spongebobMemeify(text)
+    {
+        if(text.startsWith('slackboit '))
+        {
+            text = text.split('');
+            text.splice(0,10);
+        }else{
+            text = text.split('')
+        }
+        let memed = text.map((char, index) => {
+            if (index % 2 === 0) return char.toLowerCase();
+            else return char.toUpperCase();
+        }).join('');
+
+        return memed;
+    }
+
+    static getName(username)
+    {
+        if(username.includes('logab'))
+        {
+            return 'logan-san'
+        }else if(username.includes('Dan'))
+        {
+            return 'dan-kun'
+        }else if(username.includes('Andy Miller'))
+        {
+            return 'sebrene-chan'
+        }else if(username.includes('Burtle'))
+        {
+            return 'andrew "boitle" burtle'
+        }else if(username.includes('Davon Barnette'))
+        {
+            return 'dabbo-sama'
+        }else if(username.includes('Jamz'))
+        {
+            return 'james-kun'
+        }
     }
 }
 
 const Register = [
     Messenger.goodBoit,
     Messenger.spongebobMeme,
-    Messenger.omaeWaMouShindeiru
+    Messenger.omaeWaMouShindeiru,
+    Messenger.greetingBoit
 ];
 
 module.exports = Register;
