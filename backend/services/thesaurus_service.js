14c70a99a5cefbc2e249b249261f1515ef2217c5
danqbucket <williamdanielquinones@gmail.com>
2019-09-29 19:53:07 -0400

    boit getting there

--- backend/services/thesaurus_service.js
+++ backend/services/thesaurus_service.js
@@ -4,12 +4,24 @@ const IDoThings = require('../utils/idothings')
 
 class ThesaurusService {
   // synonym of specific definition & defaults to first one,consistent
-  static async getSynonym(word, definition_id = 0) {
-    let api_call_response = await axios.get(`${SETTINGS.TESLA_MICROSERVICE_URL}location`, {
-      headers: {'x-api-key': SETTINGS.TESLA_MICROSERVICE_API_KEY}
-    })
-    if(api_call_response) return api_call_response.data
-    return null
+  static async getSynonym(word, definitionId = 0) {
+    let url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${SETTINGS.THESAURUS_API_TOKEN}`
+
+    try {
+      let resp = await axios.get(url)
+      if(!resp) return word
+    } catch (error) {
+      return word
+    }
+
+    try {
+      let arrayOfSynonymArray = resp.data.meta.syns //dunno why it return arrays inside of an array, maybe if the word has multiple meanings it returns array for each?
+      let synonym = IDoThings.pickRandomElement(arrayOfSynonymArray[definitionId])
+
+      return synonym
+    } catch (error) {
+      return word
+    }
   }
 
   // synonym of any possible definition, wild
@@ -23,11 +35,15 @@ class ThesaurusService {
       return word
     }
 
-    let arrayOfSynonymArray = resp.data.meta.syns //dunno why it return arrays inside of an array, maybe if the word has multiple meanings it returns array for each?
-    let randoArrayOfArray = IDoThings.pickRandomElement(arrayOfSynonymArray)
-    let randoSynonym = IDoThings.pickRandomElement(randoArrayOfArray)
+    try {
+      let arrayOfSynonymArray = resp.data.meta.syns //dunno why it return arrays inside of an array, maybe if the word has multiple meanings it returns array for each?
+      let randoArrayOfArray = IDoThings.pickRandomElement(arrayOfSynonymArray)
+      let randoSynonym = IDoThings.pickRandomElement(randoArrayOfArray)
 
-    return randoSynonym
+      return randoSynonym
+    } catch (error) {
+      return word
+    }
   }
 }
 
